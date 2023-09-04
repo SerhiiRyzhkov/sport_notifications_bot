c47e50fc50f4d091ce09d07d2ee4abb76071e391
SerhiiRyzhkov <had0uken@ukr.net>
2023-09-04 23:42:08 +0300

    Init a new project

---
+++ src/main/java/com/had0uken/sport_notifications_bot/SportNotificationsBotApplication.java
@@ -0,0 +1,13 @@
+package com.had0uken.sport_notifications_bot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportNotificationsBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SportNotificationsBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/had0uken/sport_notifications_bot/SportNotificationsBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.had0uken.sport_notifications_bot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SportNotificationsBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
