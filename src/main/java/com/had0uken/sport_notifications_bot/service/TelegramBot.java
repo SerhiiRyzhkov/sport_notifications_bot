922ec988e30697406b5a9baa2f1463badba36765
SerhiiRyzhkov <had0uken@ukr.net>
2023-09-05 23:59:24 +0300

    Add pom.xml, Add bot configuration

---
+++ src/main/java/com/had0uken/sport_notifications_bot/service/TelegramBot.java
@@ -0,0 +1,33 @@
+package com.had0uken.sport_notifications_bot.service;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.objects.Update;
+
+import java.util.List;
+
+@Slf4j
+@Component
+public class TelegramBot extends TelegramLongPollingBot {
+
+    @Override
+    public void onUpdateReceived(Update update) {
+
+    }
+
+    @Override
+    public void onUpdatesReceived(List<Update> updates) {
+        super.onUpdatesReceived(updates);
+    }
+
+    @Override
+    public String getBotUsername() {
+        return null;
+    }
+
+    @Override
+    public void onRegister() {
+        super.onRegister();
+    }
+}
